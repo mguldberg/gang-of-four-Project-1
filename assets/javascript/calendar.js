36a78a13582236f1d43c5dace69464ff5817732b
Laurie Aaronson <aaronson.laurie@gmail.com>
2018-03-24 10:05:35 -0500

    calendar file

---
+++ assets/javascript/calendar.js
@@ -0,0 +1,22 @@
+var CALENDAR = function () { 
+	    var wrap, label,  
+	            months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; 
+ 
+	    function init(newWrap) { 
+ 
+	    } 
+ 
+	    function switchMonth(next, month, year) { 
+ 
+	    } 
+ 
+	    function createCal(year, month) { 
+ 
+	    } 
+	    createCal.cache = {}; 
+	    return { 
+	        init : init, 
+	        switchMonth : switchMonth, 
+	        createCal   : createCal 
+	    }; 
+	};
