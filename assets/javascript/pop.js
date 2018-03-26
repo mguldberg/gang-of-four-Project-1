657ac97a1b8e1b6d5c12238e4656a21f2b17f514
Laurie Aaronson <aaronson.laurie@gmail.com>
2018-03-26 15:22:03 -0500

    pop.css

---
+++ assets/javascript/pop.js
@@ -0,0 +1,8 @@
+
+// Popup window code
+function newPopup(url) {
+	popupWindow = window.open(
+		url,'popUpWindow','height=300,width=400,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes')
+}
+
+{/* <p><a href="JavaScript:newPopup('/javascript/examples/sample_popup.cfm');">Open a popup window</a></p> */}
